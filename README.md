af64779ad0a37b5300ec3e7548a42c97da82390e
4d55397500 <svjk24@gmail.com>
2017-10-10 20:51:18 -0700

    loader class

---
+++ README.md
@@ -0,0 +1,12 @@
+object-tracker
+---
+
+**sources** holds mp4 files
+
+
+**cache** holds compressed numpy array representations of frame sequences
+
+
+**labels** holds frame sequence labels, including bounding boxes
+
+
--- objtracker/loader.py
+++ objtracker/loader.py
@@ -1,15 +1,13 @@
 # loader.py
 """
-  Load data sources to appropriate
-  data systems in GCP.
+    Load data sources to cache
 """
+import os
 import numpy as np
-import yaml
-from google.cloud import storage
 import cv2
 
 
-YAML = "./conf.yaml"
+
 
 xpixels = 480
 ypixels = 640
@@ -17,85 +15,48 @@ rgb = 3
 
 
 
-class Loader:
+
+class Loader(object):
 
     """ Load data """
 
-    def __init__(self, config):
-        self.config = config
+    def __init__(self):
+        pass
 
+    def load(self, source_path):
+        """ Load source mp4 into cache if it does not yet exist """
 
-    def load(self):
-        """ Load videos into cloud storage as compressed
-        numpy arrays. """
-        data = self.config['data']
-        local = self.config['local']
-        for kind in ['sources', 'groundtruth', 'detections']:
-            urlpath = data[kind]
-            filename = local[kind]
-            download_video(urlpath, filename)
-            arr = get_frames_array(filename)
-            print arr
+        name = source_path.split("/")[-1].split(".")[0]
+        cache_path = "cache/" + name
 
-        """
-        bucket = self.config["gcs"]["bucket"]
-        client = storage.Client()
+        if os.path.isfile(cache_path):
+            # already in cache
+            pass
+        else:
+            arr = Loader.make_frames(source_path)
+            print("Finished frame extraction for {}".format(name))
+            np.savez_compressed("cache/" + name, arr=arr)
+            print("Finished caching for {}".format(name))
 
-        bkt = client.lookup_bucket(bucket)
-        if bkt is None:
-            client.create_bucket(bucket_name=bucket)
-            bkt = client.get_bucket(bucket)       
-        """
-
-
-
-def download_video(urlpath, filename):
-    import urllib
-    print("Retrieving {}".format(urlpath))
-    urllib.urlretrieve(urlpath, filename)
-    print("Finished download")
-
-
-
-def save_arr(arr, path):
-    np.savez_compressed(path, key=arr)
-
-
-
-def get_frames_array(filename):
-
-        cap = cv2.VideoCapture(filename)
+    @staticmethod
+    def make_frames(source_path):
+        cap = cv2.VideoCapture(source_path)
         success = True
         i = 0
         images = []
+        log_period = 100
         while success:
             success, image = cap.read()
             if image is None:
                 break
             images.append(image)
             i += 1
-            print i
+            if i % log_period == 0:
+                print("Finished extracting {} frames".format(i))
         arr = np.array(images).reshape((i, xpixels, ypixels, rgb))
         return arr
 
 
 
-def read_conf():
-    with open(YAML, 'r') as fl:
-        config = yaml.load(fl)
-        return config
-
-def make_arr_unlabeled():
-    filename = "./ETH-Bahnhof.mp4"
-    arr = get_frames_array(filename)
-    np.savez_compressed("./sources", raw=arr)
-
-
-def make_arr_labeled():
-    filename = "./ETH-Bahnhof-gt.mp4"
-    arr = get_frames_array(filename)
-    np.savez_compressed("./groundtruth", groundtruth=arr)
-
-
 if __name__ == "__main__":
-   make_arr_labeled()
+    pass
